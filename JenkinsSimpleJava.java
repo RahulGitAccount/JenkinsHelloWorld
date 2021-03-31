8ad64067c725827f76089071b36dd20d6fd91284
RahulGitAccount <49607691+RahulGitAccount@users.noreply.github.com>
2021-03-31 17:09:45 +0530

    fff

--- JenkinsSimpleJava.java
+++ JenkinsSimpleJava.java
@@ -4,7 +4,7 @@ public class JenkinsSimpleJava {
 
 	public static void main(String[] args) {
 
-System.out.println("Hi have been compiled from a jenkins job");
+System.out.println("Hi have been compiled from a jenkffffins job");
 System.out.println("Updated");
 		System.out.println("Added Hoodk");
 
